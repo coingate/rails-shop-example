37b1beeabbbfd1f7a5a7a17fb1700b46a63191cc
ibaciulis <i.baciulis@gmail.com>
2015-12-29 16:46:03 +0200

    Force load .env

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module RailsShopExample
   class Application < Rails::Application
+    Dotenv.load
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
