7fb689e6bbd6a4fb39a993c1a10cc179894bccde
ibaciulis <i.baciulis@gmail.com>
2017-01-17 17:26:21 +0200

    Use API endpoints from settings

--- app/services/coingate_service.rb
+++ app/services/coingate_service.rb
@@ -4,14 +4,14 @@ class CoingateService
   end
 
   def currency_rate(from, to)
-    response = RestClient.get("https://sandbox.coingate.com/api/v1/currencies/rate/#{from}/#{to}", credentials)
+    response = RestClient.get("#{Settings.api.coingate.url}/api/v1/currencies/rate/#{from}/#{to}", credentials)
 
     JSON.parse(response.to_str)['rate'].to_f
   end
 
   def create_order(order_params = {})
     begin
-      response = RestClient.post('https://sandbox.coingate.com/api/v1/orders', order_params, credentials.merge('Content-Type' => 'application/x-www-form-urlencoded'))
+      response = RestClient.post("#{Settings.api.coingate.url}/api/v1/orders", order_params, credentials.merge('Content-Type' => 'application/x-www-form-urlencoded'))
 
       response(response.code, response.to_str)
     rescue => e
@@ -20,7 +20,7 @@ class CoingateService
   end
 
   def get_order(id)
-    response = RestClient.get("https://sandbox.coingate.com/api/v1/orders/#{id}", credentials)
+    response = RestClient.get("#{Settings.api.coingate.url}/api/v1/orders/#{id}", credentials)
 
     begin
       OpenStruct.new(success?: true, http_code: response.code, response: JSON.parse(response.to_str, symbolize_names: true))
@@ -37,7 +37,7 @@ class CoingateService
 
   def credentials
     nonce     = (Time.now.to_f * 1e6).to_i
-    message   = nonce.to_s + Settings.api.coingate.app_id.to_s + Settings.api.coingate.api_key
+    message   = "#{nonce}#{Settings.api.coingate.app_id}#{Settings.api.coingate.api_key}"
     signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), Settings.api.coingate.api_secret, message)
 
     { 'Access-Nonce' => nonce, 'Access-Key' => Settings.api.coingate.api_key, 'Access-Signature' => signature }
