e8cdddcaecb8644f589870f3b7304406199969a7
ibaciulis <i.baciulis@gmail.com>
2017-04-11 15:01:50 +0300

    Change api url

--- config/settings/application.rb
+++ config/settings/application.rb
@@ -1,7 +1,7 @@
 SimpleConfig.for :application do
   group :api do
     group :coingate do
-      set :url, "https://sandbox.coingate.com"
+      set :url, 'https://api-sandbox.coingate.com'
       set :app_id, ENV["COINGATE_APP_ID"]
       set :api_key, ENV["COINGATE_API_KEY"]
       set :api_secret, ENV["COINGATE_API_SECRET"]
